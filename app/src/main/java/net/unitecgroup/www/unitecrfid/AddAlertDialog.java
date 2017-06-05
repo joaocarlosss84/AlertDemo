5762d40f8c8f5a9461f14045c0fb0ae0c37c1218
Joao Santos <joao.santos@unitecgroup.net>
2017-06-05 15:46:03 -0300

    Initial commit

---
+++ app/src/main/java/net/unitecgroup/www/unitecrfid/AddAlertDialog.java
@@ -0,0 +1,249 @@
+package net.unitecgroup.www.unitecrfid;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static java.util.Calendar.FRIDAY;
+import static java.util.Calendar.MONDAY;
+import static java.util.Calendar.SATURDAY;
+import static java.util.Calendar.SUNDAY;
+import static java.util.Calendar.THURSDAY;
+import static java.util.Calendar.TUESDAY;
+import static java.util.Calendar.WEDNESDAY;
+
+//import android.app.DialogFragment;
+
+
+/**
+ * Created by 20006030 on 30/05/2017.
+ */
+
+public class AddAlertDialog extends DialogFragment {
+
+    public static final String ALERT_TIME = "alertTime";
+    public static final String ALERT_DURATION = "alertDuration";
+    public static final String ALERT_WEEKDAYS = "alertWeekdays";
+
+    private TextView oTextViewTime;
+    private TextView oTextViewDuration;
+    private HashSet oSelectedWeekdays = new HashSet<>();
+
+    OnAlertSavedListener oAlertListener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+
+        AlertDialog.Builder oDialogBuilder = new AlertDialog.Builder(getActivity());
+        final Bundle args = getArguments();
+
+        //Use inflater to create a new instance of the Dialog and set the view objects attributes
+        LayoutInflater inflater = (LayoutInflater) getActivity().getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View oDialogView = inflater.inflate(R.layout.add_alert_dialog, null);
+
+        //Configure Dialog with Save and Cancel buttons
+        oDialogBuilder
+                .setView(oDialogView)
+                .setIcon(R.drawable.ic_menu_camera)
+                .setTitle("Add Alert")
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // Send the event to the host activity
+                        oAlertListener.OnAlertSavedListener(
+                                oTextViewTime.getText() + " - "
+                                + oTextViewDuration.getText() + " - "
+                                + oSelectedWeekdays.toString());
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+
+        //Configure timePicker
+        //TimePicker oTimePicker = (TimePicker) oDialogView.findViewById(R.id.timePicker);
+        //oTimePicker.setIs24HourView(true);
+
+        //Configure TextViews:
+        oTextViewTime = (TextView) oDialogView.findViewById(R.id.textViewTime);
+        oTextViewDuration = (TextView) oDialogView.findViewById(R.id.textViewDuration);
+
+        if (args != null) {
+            oTextViewTime.setText(args.getString(ALERT_TIME));
+            oTextViewDuration.setText(args.getString(ALERT_DURATION));
+            ArrayList<Integer> aWeekdays = args.getIntegerArrayList(ALERT_WEEKDAYS);
+            oSelectedWeekdays.addAll(aWeekdays);
+        }
+
+        //Configure Weekday Checkboxes
+        setupWeekdays(oDialogView);
+
+
+        //Setting the Time Button OnClick
+        Button setTime;
+        setTime = (Button) oDialogView.findViewById(R.id.buttonSetTime);
+        setTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog tp1 = new TimePickerDialog(getContext(), TimePickerListener, Calendar.HOUR_OF_DAY, Calendar.MINUTE, true);
+                tp1.show();
+            }
+        });
+
+        //Setting the Time Button OnClick
+        Button setDuration;
+        setDuration = (Button) oDialogView.findViewById(R.id.buttonSetDuration);
+        setDuration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog tp1 = new TimePickerDialog(getContext(), DurationPickerListener, 0, 0, true);
+                tp1.show();
+            }
+        });
+
+        AlertDialog oAlertDialog = oDialogBuilder.create();
+
+        return oAlertDialog;
+
+    }
+
+    private void setupWeekdays(View oDialogView) {
+        LinearLayout oWeekdayGroup = (LinearLayout) oDialogView.findViewById(R.id.weekdayGroup);
+        View v = null;
+        for(int i=0; i< oWeekdayGroup.getChildCount(); i++) {
+            v = oWeekdayGroup.getChildAt(i);
+            if (v instanceof CheckBox) {
+                switch (v.getId()) {
+                    case R.id.cb_sun:
+                        v.setTag(SUNDAY);
+                        break;
+                    case R.id.cb_mon:
+                        v.setTag(MONDAY);
+                        break;
+                    case R.id.cb_tue:
+                        v.setTag(TUESDAY);
+                        break;
+                    case R.id.cb_wed:
+                        v.setTag(WEDNESDAY);
+                        break;
+                    case R.id.cb_thu:
+                        v.setTag(THURSDAY);
+                        break;
+                    case R.id.cb_fri:
+                        v.setTag(FRIDAY);
+                        break;
+                    case R.id.cb_sat:
+                        v.setTag(SATURDAY);
+                        break;
+                }
+
+                //Check the checkbox if it was selected previously
+                if (oSelectedWeekdays.contains(v.getTag())) {
+                    ((CheckBox) v).setChecked(true);
+                }
+
+                //Bind our SelectedWeekdays with checkboxes
+                ((CheckBox) v).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        // update your model (or other business logic) based on isChecked
+                        if (isChecked) {
+                            oSelectedWeekdays.add(buttonView.getTag());
+                        } else {
+                            oSelectedWeekdays.remove(buttonView.getTag());
+                        }
+                    }
+                });
+
+            }
+        }
+    }
+
+    //Restores the DialogFragment values
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            //Restore the fragment's state here
+            oTextViewTime.setText(savedInstanceState.getCharSequence(ALERT_TIME));
+            oTextViewDuration.setText(savedInstanceState.getCharSequence(ALERT_DURATION));
+
+            ArrayList<Integer> aWeekdays = savedInstanceState.getIntegerArrayList(ALERT_WEEKDAYS);
+            oSelectedWeekdays.addAll(aWeekdays);
+        }
+    }
+
+    //Save the DialogFrament values
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putCharSequence(ALERT_TIME, oTextViewTime.getText());
+        outState.putCharSequence(ALERT_DURATION, oTextViewDuration.getText());
+
+        //Convert HashSet to ArrayList and save it to State
+        Integer[] aInt = (Integer[]) oSelectedWeekdays.toArray(new Integer[oSelectedWeekdays.size()]);
+        ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(aInt));
+        outState.putIntegerArrayList(ALERT_WEEKDAYS, arr);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    // Container Activity must implement this interface
+    public interface OnAlertSavedListener {
+        public void OnAlertSavedListener(String alertTime);
+    }
+
+    //Attach the activy that call AddAlertDialog to callback
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            oAlertListener = (OnAlertSavedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement OnAlertSavedListener");
+        }
+    }
+
+    private TimePickerDialog.OnTimeSetListener TimePickerListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    oTextViewTime.setText( String.format("%02d:%02d", hourOfDay, minute) );
+                    return;
+                }
+            };
+
+    private TimePickerDialog.OnTimeSetListener DurationPickerListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    oTextViewDuration.setText( String.format("%02d:%02d", hourOfDay, minute) );
+                    return;
+                }
+            };
+
+
+}
+
---
+++ app/src/main/java/net/unitecgroup/www/unitecrfid/MainActivity.java
@@ -0,0 +1,344 @@
+package net.unitecgroup.www.unitecrfid;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static net.unitecgroup.www.unitecrfid.AddAlertDialog.ALERT_DURATION;
+import static net.unitecgroup.www.unitecrfid.AddAlertDialog.ALERT_TIME;
+import static net.unitecgroup.www.unitecrfid.AddAlertDialog.ALERT_WEEKDAYS;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        AddAlertDialog.OnAlertSavedListener {
+
+    AlertListAdapter oAlertListAdapter;
+    RecyclerView mRecyclerView;
+
+    FragmentManager fm = getSupportFragmentManager();
+    AddAlertDialog oAddAlert;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Floating Action Button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                oAddAlert = new AddAlertDialog();
+                oAddAlert.show(fm, "Dialog Fragment");
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.mainListView);
+        setUpRecyclerView();
+
+    }
+
+    private void setUpRecyclerView() {
+        oAlertListAdapter = new AlertListAdapter();
+        oAlertListAdapter.setUndoOn(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(oAlertListAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        oAlertListAdapter.SetOnItemClickListener(
+            new AlertListAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(View view, int position, String id) {
+                    Bundle args = new Bundle(); //Bundle containing data you are passing to the dialog
+                    args.putString(ALERT_TIME, "06:30");
+                    args.putString(ALERT_DURATION, "00:15");
+                    args.putIntegerArrayList(ALERT_WEEKDAYS, new ArrayList<Integer>(Arrays.asList(2,6)));
+
+                    oAddAlert = new AddAlertDialog();
+                    oAddAlert.setArguments(args);
+                    oAddAlert.show(fm, "Dialog Fragment");
+                }
+            }
+        );
+
+        setUpItemTouchHelper();
+        setUpAnimationDecoratorHelper();
+    }
+
+
+    /**
+     * This is the standard support library way of implementing "swipe to delete" feature. You can do custom drawing in onChildDraw method
+     * but whatever you draw will disappear once the swipe is over, and while the items are animating to their new position the recycler view
+     * background will be visible. That is rarely an desired effect.
+     */
+    private void setUpItemTouchHelper() {
+
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // we want to cache these and not allocate anything repeatedly in the onChildDraw method
+            Drawable background;
+            Drawable xMark;
+            int xMarkMargin;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                xMark = ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_clear_24dp);
+                xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                xMarkMargin = (int) MainActivity.this.getResources().getDimension(R.dimen.fab_margin);
+                initiated = true;
+            }
+
+            // not important, we don't want drag & drop
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                AlertListAdapter testAdapter = (AlertListAdapter)recyclerView.getAdapter();
+                if (testAdapter.isUndoOn() && testAdapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                AlertListAdapter adapter = (AlertListAdapter)mRecyclerView.getAdapter();
+                boolean undoOn = adapter.isUndoOn();
+                if (undoOn) {
+                    adapter.pendingRemoval(swipedPosition);
+                } else {
+                    adapter.remove(swipedPosition);
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.getAdapterPosition() == -1) {
+                    // not interested in those
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                // draw x mark
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+                int intrinsicWidth = xMark.getIntrinsicWidth();
+                int intrinsicHeight = xMark.getIntrinsicWidth();
+
+                int xMarkLeft = itemView.getRight() - xMarkMargin - intrinsicWidth;
+                int xMarkRight = itemView.getRight() - xMarkMargin;
+                int xMarkTop = itemView.getTop() + (itemHeight - intrinsicHeight)/2;
+                int xMarkBottom = xMarkTop + intrinsicHeight;
+                xMark.setBounds(xMarkLeft, xMarkTop, xMarkRight, xMarkBottom);
+
+                xMark.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+        };
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    /**
+     * We're gonna setup another ItemDecorator that will draw the red background in the empty space while the items are animating to thier new positions
+     * after an item is removed.
+     */
+    private void setUpAnimationDecoratorHelper() {
+        mRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+
+            // we want to cache this and not allocate anything repeatedly in the onDraw method
+            Drawable background;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                initiated = true;
+            }
+
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+
+                if (!initiated) {
+                    init();
+                }
+
+                // only if animation is in progress
+                if (parent.getItemAnimator().isRunning()) {
+
+                    // some items might be animating down and some items might be animating up to close the gap left by the removed item
+                    // this is not exclusive, both movement can be happening at the same time
+                    // to reproduce this leave just enough items so the first one and the last one would be just a little off screen
+                    // then remove one from the middle
+
+                    // find first child with translationY > 0
+                    // and last one with translationY < 0
+                    // we're after a rect that is not covered in recycler-view views at this point in time
+                    View lastViewComingDown = null;
+                    View firstViewComingUp = null;
+
+                    // this is fixed
+                    int left = 0;
+                    int right = parent.getWidth();
+
+                    // this we need to find out
+                    int top = 0;
+                    int bottom = 0;
+
+                    // find relevant translating views
+                    int childCount = parent.getLayoutManager().getChildCount();
+                    for (int i = 0; i < childCount; i++) {
+                        View child = parent.getLayoutManager().getChildAt(i);
+                        if (child.getTranslationY() < 0) {
+                            // view is coming down
+                            lastViewComingDown = child;
+                        } else if (child.getTranslationY() > 0) {
+                            // view is coming up
+                            if (firstViewComingUp == null) {
+                                firstViewComingUp = child;
+                            }
+                        }
+                    }
+
+                    if (lastViewComingDown != null && firstViewComingUp != null) {
+                        // views are coming down AND going up to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    } else if (lastViewComingDown != null) {
+                        // views are going down to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = lastViewComingDown.getBottom();
+                    } else if (firstViewComingUp != null) {
+                        // views are coming up to fill the void
+                        top = firstViewComingUp.getTop();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    }
+
+                    background.setBounds(left, top, right, bottom);
+                    background.draw(c);
+
+                }
+                super.onDraw(c, parent, state);
+            }
+
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //Executed once the AddAlert is saved
+    @Override
+    public void OnAlertSavedListener(String alertTime) {
+        oAlertListAdapter.addAlert(alertTime);
+    }
+}
